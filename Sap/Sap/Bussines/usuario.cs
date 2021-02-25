69abd8ce50be31135efe244858232c45ab9ee284
cloudprog <gabrod7k@gmail.com>
2021-02-25 16:24:52 -0300

    250221

---
+++ Sap/Sap/Bussines/usuario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sap.Bussines
+{
+    class usuario
+    {
+
+        Database.usuario banco = new Database.usuario();
+        public void inserir(Database.Entities.usuario dados)
+       {
+            
+            banco.inserir(dados);
+            if (dados.nick == string.Empty)
+            { throw new Exception("confira seu formulario"); }
+        }
+        public List<Database.Entities.usuario> btodos()
+        {
+            List<Database.Entities.usuario> lista = banco.btodos();
+            return lista;
+
+        }
+        public List<Database.Entities.usuario> bnick(string nick)
+        {
+            List<Database.Entities.usuario> lista = banco.bnick(nick);
+            return lista;
+        }
+
+        public void delete(string nick)
+        {
+            banco.deletar(nick);
+        }
+
+        public void alterar(Database.Entities.usuario dados)
+        {
+            banco.alterar(dados);
+            if (dados == null)
+            { throw new Exception("erro, por favor verifique seu formulario"); }
+
+        }
+
+        public
+        Database.Entities.usuario busca(string nick)
+        {
+
+            Database.Entities.usuario lista = banco.buscar(nick);
+            return lista;
+        }
+
+    }
+}
---
+++ Sap/Sap/Database/usuario.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sap.Database
+{
+    class usuario
+    {
+        Entities.estudoEntities banco = new Entities.estudoEntities();
+        public void inserir(Entities.usuario dados)
+        {
+            banco.usuario.Add(dados);
+            banco.SaveChanges();
+
+        }
+
+        public Entities.usuario buscar(string nick)
+        {
+            Entities.usuario lista = banco.usuario.FirstOrDefault(t => t.nick == nick);
+            return lista;
+        }
+
+        public List<Entities.usuario> btodos()
+        {
+            List<Entities.usuario> lista = banco.usuario.OrderByDescending(t => t.registro).ToList();
+            return lista;
+        }
+
+        public List<Entities.usuario> bnick(string nick)
+        {
+            List<Entities.usuario> lista = banco.usuario.Where(t => t.nick == nick).ToList();
+            return lista;
+
+        }
+
+        public void deletar(string nick)
+        {
+            Entities.usuario lista = banco.usuario.FirstOrDefault(t => t.nick == nick);
+            banco.Entry(lista).State = System.Data.Entity.EntityState.Deleted;
+            banco.SaveChanges();
+        }
+
+        public void alterar(Entities.usuario nick)
+        {
+            Entities.usuario usuario = banco.usuario.FirstOrDefault(t => t.nick == nick.nick);
+
+            if (usuario != null)
+            {
+                usuario.cargo = nick.cargo;
+
+                usuario.dt_nascimento = nick.dt_nascimento;
+
+                usuario.foto = nick.foto;
+                usuario.idade = nick.idade;
+
+                usuario.nome = nick.nome;
+                usuario.registro = nick.registro;
+                usuario.nick = nick.nick;
+
+                banco.SaveChanges();
+            }
+
+        }
+    }
+}
