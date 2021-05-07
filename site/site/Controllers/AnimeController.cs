1f73bcf7b673b119f749efced06818d5245e3b14
cloudprog <gabrod7k@gmail.com>
2021-05-07 13:20:38 -0300

    a

--- site/site/Controllers/AnimeController.cs
+++ site/site/Controllers/AnimeController.cs
@@ -15,16 +15,74 @@ namespace site.Controllers
         }
 
        [HttpPost]
-        public ActionResult inserir(model.venda model)
+        public ActionResult admcadastro(model.administrador model)
         {
-       business.vendasbusiness bvenda = new business.vendasbusiness();
+       business.administradorbusiness badm = new business.administradorbusiness();
+            badm.inserir(model);
+            return View();
+        }
+        public ActionResult admcadastro()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult clientecadastro (model.cliente model)
+        {
+            business.clientebusiness bcliente = new business.clientebusiness();
+            bcliente.inserir(model);
+            return View();
+        }
+        public ActionResult clientecadastro()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult logincadastro(model.login model)
+        {
+            business.loginbusiness blogin = new business.loginbusiness();
+            blogin.inserir(model);
+            return View();
+        }
+        public ActionResult logincadastro()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult produtocadastro(model.produto model)
+        {
+            business.produtosbusiness bproduto = new business.produtosbusiness();
+            bproduto.inserir(model);
+            return View();
+        }
+        public ActionResult produtocadastro()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult vendacadastro(model.venda model)
+        {
+            business.vendasbusiness bvenda = new business.vendasbusiness();
             bvenda.inserir(model);
             return View();
         }
-        public ActionResult inserir()
+        public ActionResult vendacadastro()
         {
             return View();
         }
+
+
+
         public ActionResult lista()
         {
             business.vendasbusiness bvendas = new business.vendasbusiness();
