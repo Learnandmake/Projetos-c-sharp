38643a092ff471ef143f505a76654dae5c2ca633
cloudprog <gabrod7k@gmail.com>
2021-05-07 07:49:06 -0300

    criptografia e verificacao

---
+++ site/site/Functions/verificar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace site.Functions
+{
+    public class verificar
+    {
+        //calcular idade
+        public int calcularidade(DateTime nascimento)
+        {
+            int idade = DateTime.Now.Year - nascimento.Year;
+            if (DateTime.Now.DayOfYear < nascimento.DayOfYear)
+            {
+                idade--;
+
+            }
+            return idade;
+        }
+        public bool controlenumerico(string nusuario)//numero
+        {
+            int numero = 0;
+            bool teste = int.TryParse(nusuario, out numero);
+            return teste;
+        }
+
+
+        //verificacao de  idade 
+        public bool verificaridade(DateTime dt_nascimento)
+        {
+            int idade = DateTime.Now.Year - dt_nascimento.Year;
+            bool ok = false;
+            if (DateTime.Now.DayOfYear < dt_nascimento.DayOfYear)
+            {
+                idade--;
+
+
+                if (idade < 18)
+                {
+                    ok = false;
+
+                }
+            }
+            if (idade >= 18)
+            {
+                ok = true;
+
+            }
+            return ok;
+        }
+
+
+        public bool isemail(string strEmail)
+        {
+            string strModelo = "^([0-9a-zA-Z]([-.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+            if (System.Text.RegularExpressions.Regex.IsMatch(strEmail, strModelo))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
